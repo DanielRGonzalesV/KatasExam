54d6c525eb03c1bd322a7597d58d1c45b2a54845
DanielRGonzalesV <drgvandroid@gmail.com>
2016-07-25 19:53:18 -0400

    Kata two

---
+++ src/main/java/org/fundacionjala/katas/SearchEngine.java
@@ -0,0 +1,29 @@
+package org.fundacionjala.katas;
+
+/**
+ * Created by danielgonzales on 7/25/2016.
+ */
+public class SearchEngine {
+
+
+    private static final int INT_ZERO = 0;
+    private static final int INT_ONE = 1;
+
+    public static int find (String needle, String haystack) {
+
+        retval = str2.contains("_");
+
+
+        if( haystack.contains( "_" ) ) {
+            System.out.println ("contiene _");
+            return 5;
+//            for(int i = INT_ZERO; i < haystack.length (); i++ ){
+//
+//            }
+        }
+        else{
+            return haystack.indexOf (needle);
+        }
+
+    }
+    }
